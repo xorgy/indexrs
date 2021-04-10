4585827bd386353940bba39f23f18a5a417f9465
Aaron Muir Hamilton <aaron@correspondwith.me>
2021-04-10 04:42:40 +0000

    Add MergeIndex.

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ use unicode_normalization::UnicodeNormalization;
 #[cfg(test)]
 mod tests {
     #[test]
-    fn integer_keys() {
+    fn integer_keys_invert() {
         let mut baar = crate::InvertIndex::<u32>::default();
         baar.insert(69, "boof");
         baar.insert(420, "foob");
@@ -25,7 +25,7 @@ mod tests {
     }
 
     #[test]
-    fn string_keys() {
+    fn str_keys_invert() {
         let mut quux = crate::InvertIndex::<&str>::default();
         quux.insert("pleased", "boof");
         quux.insert("blazed", "foob");
@@ -37,8 +37,78 @@ mod tests {
         assert_eq!(quux.index, baaz.index);
         assert_eq!(quux.query("oof"), baaz.query("oof"));
     }
+
+    #[test]
+    fn integer_keys_merge() {
+        let mut baar = crate::MergeIndex::<u32>::default();
+        baar.insert(69, "boof");
+        baar.insert(420, "foob");
+
+        let mut fooo = crate::MergeIndex::<u32>::default();
+        fooo.insert(420, "foob");
+        fooo.insert(69, "boof");
+
+        assert_eq!(baar.index, fooo.index);
+        assert_eq!(baar.query("oof"), fooo.query("oof"));
+    }
+
+    #[test]
+    fn str_keys_merge() {
+        let mut quux = crate::MergeIndex::<&str>::default();
+        quux.insert("pleased", "boof");
+        quux.insert("blazed", "foob");
+
+        let mut baaz = crate::MergeIndex::<&str>::default();
+        baaz.insert("blazed", "foob");
+        baaz.insert("pleased", "boof");
+
+        assert_eq!(quux.index, baaz.index);
+        assert_eq!(quux.query("oof"), baaz.query("oof"));
+    }
+
+    #[test]
+    fn invert_vs_merge() {
+        let mut baar = crate::InvertIndex::<u32>::default();
+        baar.insert(69, "boof");
+        baar.insert(420, "foob");
+
+        let mut fooo = crate::MergeIndex::<u32>::default();
+        fooo.insert(420, "foob");
+        fooo.insert(69, "boof");
+
+        assert_eq!(baar.query("oof"), fooo.query("oof"));
+    }
+
+    #[test]
+    fn invert_vs_merge_from() {
+        let mut baar = crate::InvertIndex::<u32>::default();
+        baar.insert(69, "boof");
+        baar.insert(420, "foob");
+
+        let fooo = crate::MergeIndex::from(baar.clone());
+
+        assert_eq!(baar.query("oof"), fooo.query("oof"));
+    }
+
+    #[test]
+    fn merge_vs_invert_from() {
+        let mut baar = crate::MergeIndex::<u32>::default();
+        baar.insert(69, "boof");
+        baar.insert(420, "foob");
+
+        let fooo = crate::InvertIndex::from(baar.clone());
+
+        assert_eq!(baar.query("oof"), fooo.query("oof"));
+    }
 }
 
+#[derive(Clone)]
+pub struct MergeIndex<T> {
+    depth: usize,
+    index: HashMap<T, HashSet<String>>,
+}
+
+#[derive(Clone)]
 pub struct InvertIndex<T> {
     depth: usize,
     index: HashMap<String, HashSet<T>>,
@@ -88,6 +158,91 @@ where
         let mut inter = accum_map.iter().collect::<Vec<(&T, &u32)>>();
         inter.sort_by(|a, b| a.1.cmp(&b.1).reverse());
         inter.iter().map(|x| *x.0).collect::<Vec<T>>()
+    }
+}
+
+impl<T> MergeIndex<T>
+where
+    T: Hash,
+    T: Eq,
+    T: Copy,
+{
+    pub fn default() -> Self {
+        MergeIndex::new(6)
+    }
+
+    pub fn new(depth: usize) -> Self {
+        MergeIndex {
+            depth: depth,
+            index: HashMap::<T, HashSet<String>>::new(),
+        }
+    }
+
+    pub fn insert(&mut self, key: T, index_string: &str) {
+        let gs = mkgrams(index_string, self.depth);
+        self.index.insert(
+            key,
+            match self.index.get(&key) {
+                Some(kg) => gs.union(&kg).map(|x| String::from(x)).collect(),
+                None => gs,
+            },
+        );
+    }
+
+    pub fn query(&self, query_string: &str) -> Vec<T> {
+        let gs = mkgrams(query_string, self.depth);
+        let mut inter = self
+            .index
+            .iter()
+            .map(|x| (*x.0, x.1.intersection(&gs).count()))
+            .filter(|x| x.1 != 0)
+            .collect::<Vec<(T, usize)>>();
+        inter.sort_by(|a, b| a.1.cmp(&b.1).reverse());
+        inter.iter().map(|x| x.0).collect::<Vec<T>>()
+    }
+}
+
+impl<T> From<MergeIndex<T>> for InvertIndex<T>
+where
+    T: Hash,
+    T: Eq,
+    T: Copy,
+{
+    fn from(merge_index: MergeIndex<T>) -> Self {
+        let mut index = HashMap::<String, HashSet<T>>::new();
+        for (key, gs) in merge_index.index {
+            for gram in gs.iter() {
+                let entry = index
+                    .entry(gram.to_owned())
+                    .or_insert_with(HashSet::<T>::new);
+                entry.insert(key);
+            }
+        }
+        InvertIndex {
+            depth: merge_index.depth,
+            index: index,
+        }
+    }
+}
+
+impl<T> From<InvertIndex<T>> for MergeIndex<T>
+where
+    T: Hash,
+    T: Eq,
+    T: Copy,
+{
+    fn from(invert_index: InvertIndex<T>) -> Self {
+        let mut index = HashMap::<T, HashSet<String>>::new();
+        for (gram, ks) in invert_index.index {
+            for key in ks.iter() {
+                let entry = index.entry(*key).or_insert_with(HashSet::<String>::new);
+                entry.insert(gram.clone());
+            }
+        }
+        MergeIndex {
+            depth: invert_index.depth,
+            index: index,
+        }
     }
 }
 
